03e779a3bc1009f0542cad2d2bdc64e86fd3cd6f
Valerij15 <valerij.val@gmail.com>
2021-08-31 19:15:16 +0200

    Dodan seznam dogodkov ob dnevu.

--- model.py
+++ model.py
@@ -2,15 +2,52 @@ import datetime
 from calendar import monthrange
 from dateutil.relativedelta import relativedelta
 
+class Koledar:
+
+    def __init__(self, datum, dogodki):
+        self.datum = datum
+        self.dogodki = dogodki
+        self.tabela_datumov = self.naredi_tabelo_datumov()
+        self.vklopljen = (False, 0)
+
+    def dodaj_mesece(self, n):
+        self.datum.dodaj_mesece(n)
+        self.tabela_datumov = self.naredi_tabelo_datumov()
+
+    def vklopi(self, i):
+        if self.vklopljen[1] == i:
+            self.vklopljen = (not (self.vklopljen[0]), i)
+        else:
+            self.vklopljen = (self.vklopljen[0], i)
+
+    def naredi_tabelo_datumov(self):
+        tabela = []
+        prvi_teden = Datum(datetime.date(self.datum.leto, self.datum.mesec, 1))
+        zacetek_meseca = prvi_teden.stevilo_dneva_v_tednu()
+        konec_meseca = self.datum.stevilo_dni_v_mesecu() + zacetek_meseca - 1
+        prejsnji_teden = Datum(datetime.date(prvi_teden.leto, prvi_teden.mesec, prvi_teden.dan))
+        prejsnji_teden.dodaj_dneve(-(zacetek_meseca - 1))
+        naslednji_teden = Datum(datetime.date(self.datum.leto, self.datum.mesec, self.datum.stevilo_dni_v_mesecu()))
+        naslednji_teden.dodaj_dneve(1)
+        for i in range(1, 43):
+            if i < zacetek_meseca:
+                tabela.append((prejsnji_teden.dan, prejsnji_teden.ime_meseca(), prejsnji_teden.leto,  0))
+                prejsnji_teden.dodaj_dneve(1)
+            elif i > konec_meseca:
+                tabela.append((naslednji_teden.dan, naslednji_teden.ime_meseca(), naslednji_teden.leto, 0))
+                naslednji_teden.dodaj_dneve(1)
+            else:
+                tabela.append((i- zacetek_meseca + 1, self.datum.ime_meseca(), self.datum.leto, 1))
+        return tabela
+
+
 class Datum:
 
-    def __init__(self):
-        self.datum = self.danasnji_datum()
-        self.stevilo_dneva_v_tednu = int(self.datum.strftime("%w"))
+    def __init__(self, datum = datetime.datetime.now()):
+        self.datum = datum
         self.dan = int(self.datum.strftime("%d"))
         self.mesec = int(self.datum.strftime("%m"))
         self.leto = int(self.datum.strftime("%Y"))
-        self.tabela_datumov = self.naredi_tabelo()
         
     def danasnji_datum(self):
         return datetime.datetime.now()
@@ -18,53 +55,38 @@ class Datum:
     def stevilo_dni_v_mesecu(self):
         return monthrange(self.leto, self.mesec)[1]
     
+    def stevilo_dneva_v_tednu(self):
+        n = int(self.datum.strftime("%w"))
+        if n == 0:
+            n = 7
+        return n
+
     def ime_meseca(self):
         meseci = ["Januar", "Februar", "Marec", "April", "Maj", "Junij", "Julij", "Avgust", "September", "Oktober", "November", "December"]
         return meseci[self.mesec - 1]
         
-    def zacetek_meseca(self):
-        return 8 + (self.stevilo_dneva_v_tednu - (self.dan % 7))
-
     def dodaj_mesece(self, n):
         self.datum = self.datum + relativedelta(months=n)
         self.posodobi_spremenljivke()
+
+    def dodaj_dneve(self, n):
+        self.datum = self.datum + relativedelta(days=n)
+        self.posodobi_spremenljivke()
     
     def posodobi_spremenljivke(self):
-        self.stevilo_dneva_v_tednu = int(self.datum.strftime("%w"))
         self.dan = int(self.datum.strftime("%d"))
         self.mesec = int(self.datum.strftime("%m"))
         self.leto = int(self.datum.strftime("%Y"))
-        self.tabela_datumov = self.naredi_tabelo()
     
-    def naredi_tabelo(self):
-        tabela = []
-        prvi_teden = datetime.date(self.leto, self.mesec, 1)
-        zacetek_meseca = int(prvi_teden.strftime("%w"))
-        if zacetek_meseca == 0:
-            zacetek_meseca = 7
-        konec_meseca = self.stevilo_dni_v_mesecu() + zacetek_meseca - 1
-        prejsnji_teden = prvi_teden + relativedelta(days = -(zacetek_meseca - 1))
-        naslednji_teden = datetime.date(self.leto, self.mesec, self.stevilo_dni_v_mesecu()) + relativedelta(days = 1)
-        for i in range(1, 43):
-            if i < zacetek_meseca:
-                tabela.append((int(prejsnji_teden.strftime("%d")), 0))
-                prejsnji_teden += relativedelta(days = 1)
-            elif i > konec_meseca:
-                tabela.append((int(naslednji_teden.strftime("%d")), 0))
-                naslednji_teden += relativedelta(days = 1)
-            else:
-                if(self.mesec == int(self.danasnji_datum().strftime("%m")) and self.leto == int(self.danasnji_datum().strftime("%Y")) and (i- zacetek_meseca + 1) == int(self.danasnji_datum().strftime("%d")) ):
-                    tabela.append((i- zacetek_meseca + 1, 2))
-                else:
-                    tabela.append((i- zacetek_meseca + 1, 1))
-        return tabela
 
 
 class Uporabnik:
 
     def __init__(self, username):
         self.username = username
+        self.datum = Datum()
         self.dogodki = []
+        self.koledar = Koledar(self.datum, self.dogodki)
 
     def dodaj_dogodek(self, ime, datum, opis = ''):
         self.dogodki.append(Dogodek(ime, datum, opis))
@@ -81,4 +103,4 @@ class Dogodek:
         self.datum = datum
         self.opis = opis
 
-poskus = Datum()
+poskus = Uporabnik("Valerij")
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -4,16 +4,21 @@ import model
 
 @bottle.route('/')
 def osnovna_stran():
-    return bottle.template("osnovna_stran.html", mesec= model.poskus.ime_meseca(), leto = model.poskus.leto, tabela = model.poskus.tabela_datumov)
+    return bottle.template("osnovna_stran.html", mesec= model.poskus.datum.ime_meseca(), leto = model.poskus.datum.leto, tabela = model.poskus.koledar.tabela_datumov, vklopljen = model.poskus.koledar.vklopljen)
 
 @bottle.get('/naslednji_mesec/')
 def naslednji_mesec():
-    model.poskus.dodaj_mesece(1)
+    model.poskus.koledar.dodaj_mesece(1)
     bottle.redirect("/")
 
 @bottle.get('/prejsnji_mesec/')
 def prejsnji_mesec():
-    model.poskus.dodaj_mesece(-1)
+    model.poskus.koledar.dodaj_mesece(-1)
+    bottle.redirect("/")
+
+@bottle.get('/vklopi<i>/')
+def vklopi(i):
+    model.poskus.koledar.vklopi(int(i))
     bottle.redirect("/")
 
 bottle.run(debugger= True)
